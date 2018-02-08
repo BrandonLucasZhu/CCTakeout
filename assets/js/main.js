7b42318ab5e7c35056f84c99e56dc6e16f77f660
Brandon Zhu <brandonlucaszhu@gmail.com>
2018-02-08 16:56:43 -0500

    Testing init dom manipulations

--- assets/js/main.js
+++ assets/js/main.js
@@ -37,8 +37,39 @@ var UIController = (function(){
            var request = new XMLHttpRequest();    
            request.open("GET", "http://localhost:3000/db", false);
            request.send(null)
-           var my_JSON_object = JSON.parse(request.responseText);
-           console.log(my_JSON_object.appetizers);
+           var menuJSON = JSON.parse(request.responseText);
+            var reverseKeys = Object.keys(menuJSON).reverse();
+           var appetizerss = JSON.stringify(menuJSON);
+            var i = 0;
+            var elementStore = '<div class="row">                                                                                            <div id="food-id" class="col-md-9"><p>%name%</p></div>                                              <div id="food-id2" class="col-md-3"><p>$%price%</p></div>                                       </div>';
+            
+            
+            for (var i = 0; i < reverseKeys.length; i++) {
+                for (var j = menuJSON[reverseKeys[i]].length - 1; j >= 0; j--) {
+                    //Add name of the dish
+                    var newElement = elementStore.replace("%name%", JSON.stringify(menuJSON[reverseKeys[i]][j]["name"]).replace(/^"(.*)"$/, '$1')); //Remove quotation marks in key
+                    
+                    if (Array.isArray(menuJSON[reverseKeys[i]][j]["price"])){
+                    
+                    //Add the price of the dish
+                    newElement = newElement.replace("%price%", JSON.stringify(menuJSON[reverseKeys[i]][j]["price"][0]) +" $" + JSON.stringify(menuJSON[reverseKeys[i]][j]["price"][1]) );
+                    }
+                    else {
+                        newElement = newElement.replace("%price%", JSON.stringify(menuJSON[reverseKeys[i]][j]["price"]));    
+                    }
+                        
+                        
+                    //Insert the HTML into the DOM .insertAdjacentHTML('afterend', "<div><p>hi</p></div>");
+                    document.getElementById("menu--title").insertAdjacentHTML('afterend', newElement);
+                }
+                
+            }
+            
+            
+            
+            
+            
+            
         }
         
     }; 
@@ -54,6 +85,11 @@ var controller = (function(foodValueCtrl, UICtrl){
     
     return {
         
+        startMenu: function(){
+            UICtrl.initMenus()
+        }
     };
     
 })(foodValueController, UIController);
+
+controller.startMenu();
