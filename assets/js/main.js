6b2b0d51b5e25a265ff8e9034ee2b4baa7971146
Brandon Lucas Zhu <Brandonlucaszhu@gmail.com>
2018-02-07 20:55:38 -0500

    tested json menu and, set up JS controllers

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,29 @@
+//ES5 JS
+
+//Private: Use to calculate price
+var foodValueController = (function(){ 
+
+    var id, name, price, size, description;
+    
+    //Constructor for food fields
+    var foodSpecs = function(id, name, price, size, description) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.size = size;
+        this.description = description;
+    };
+
+    return {
+        
+    };
+    
+})();
+
 //UI CONTROLLER: Private
-var UI = (function(){
+var UIController = (function(){
+    
+    
     
     
     
@@ -7,14 +31,29 @@ var UI = (function(){
    
     
     return {
-        
+         
+        //Display the entire menu initially
         initMenus: function() {
-           
-            var jsonMenu = JSON.parse(menu);
-            console.log(jsonMenu);
+           var request = new XMLHttpRequest();    
+           request.open("GET", "http://localhost:3000/db", false);
+           request.send(null)
+           var my_JSON_object = JSON.parse(request.responseText);
+           console.log(my_JSON_object.appetizers);
         }
         
     }; 
 })();
 
-UI.initMenus();
+
+
+//Main global controller private
+var controller = (function(foodValueCtrl, UICtrl){
+    
+    
+    
+    
+    return {
+        
+    };
+    
+})(foodValueController, UIController);
