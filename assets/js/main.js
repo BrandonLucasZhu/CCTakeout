bf82436937def23f3f47a30208fd01debeab4c82
Brandon Zhu <brandonlucaszhu@gmail.com>
2018-02-09 11:46:36 -0500

    General Layout of "Home" and "Menu" page

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,8 @@ var foodValueController = (function(){
 
     var id, name, price, size, description;
     
+    
+    
     //Constructor for food fields
     var foodSpecs = function(id, name, price, size, description) {
         this.id = id;
@@ -23,7 +25,7 @@ var foodValueController = (function(){
 //UI CONTROLLER: Private
 var UIController = (function(){
     
-    
+    var categories = ["Combination Plates", "Dinners", "Specialities", "SeaFood", "Sweet and Sour", "Chow Mein/Chop Suey", "Chicken", "Egg Foo Young", "Vegetable Dishes", "Beef and Pork", "Fried Rice", "Side Orders", "Soups", "Appetizers"]
     
     
     
@@ -34,14 +36,14 @@ var UIController = (function(){
          
         //Display the entire menu initially
         initMenus: function() {
-           var request = new XMLHttpRequest();    
-           request.open("GET", "http://localhost:3000/db", false);
-           request.send(null)
-           var menuJSON = JSON.parse(request.responseText);
+            var request = new XMLHttpRequest();    
+            request.open("GET", "http://localhost:3000/db", false);
+            request.send(null)
+            var menuJSON = JSON.parse(request.responseText);
             var reverseKeys = Object.keys(menuJSON).reverse();
-           var appetizerss = JSON.stringify(menuJSON);
-            var i = 0;
-            var elementStore = '<div class="row">                                                                                            <div id="food-id" class="col-md-9"><p>%name%</p></div>                                              <div id="food-id2" class="col-md-3"><p>$%price%</p></div>                                       </div>';
+            var elementStore = '<div class="row">                                                                                            <div id="food-id" class="col-md-9"><p>%name%</p></div>                                              <div id="food-id2" class="col-md-3"><p>$%price%</p></div>                                                  </div><div class="row"><div class="col-md-12"><p>%description%</p></div></div>';
+            
+            var subCategoryHeading = '<div id="%idCate%" class="row"><h3 class="center">%categoryHeading%</h3></div>';
             
             
             for (var i = 0; i < reverseKeys.length; i++) {
@@ -51,17 +53,29 @@ var UIController = (function(){
                     
                     if (Array.isArray(menuJSON[reverseKeys[i]][j]["price"])){
                     
-                    //Add the price of the dish
-                    newElement = newElement.replace("%price%", JSON.stringify(menuJSON[reverseKeys[i]][j]["price"][0]) +" $" + JSON.stringify(menuJSON[reverseKeys[i]][j]["price"][1]) );
+                        //Add the price of the dish
+                        newElement = newElement.replace("%price%", menuJSON[reverseKeys[i]][j]["price"][0].toFixed(2) +" $" + menuJSON[reverseKeys[i]][j]["price"][1].toFixed(2));
                     }
                     else {
-                        newElement = newElement.replace("%price%", JSON.stringify(menuJSON[reverseKeys[i]][j]["price"]));    
+                        newElement = newElement.replace("%price%", menuJSON[reverseKeys[i]][j]["price"].toFixed(2));
                     }
-                        
-                        
-                    //Insert the HTML into the DOM .insertAdjacentHTML('afterend', "<div><p>hi</p></div>");
+                    
+                    //Check if description is apparent in JSON property
+                    if (menuJSON[reverseKeys[i]][j].hasOwnProperty("description")){
+                        newElement = newElement.replace("%description%", JSON.stringify(menuJSON[reverseKeys[i]][j]["description"]).replace(/^"(.*)"$/, '$1'));
+                    }
+                    else{
+                            newElement = newElement.replace("%description%", "");
+                    }
+                           
+                    //Insert the HTML into the DOM 
                     document.getElementById("menu--title").insertAdjacentHTML('afterend', newElement);
                 }
+                
+                //Add subheadings after all the foods have been appended in that section
+                var categoryHeading = subCategoryHeading.replace("%categoryHeading%", categories[i]);
+                categoryHeading = categoryHeading.replace("%idCate%", categories[i]);
+                document.getElementById("menu--title").insertAdjacentHTML('afterend', categoryHeading);
                 
             }
             
